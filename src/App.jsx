780547b987de017e11316a37c9b8b40deee29b28
lokendra pandey <lokendrapandey5278@gmail.com>
2024-08-09 17:48:27 +0530

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import './App.css'
+// import './App.css'
 import authService from "./appwrite/auth"
 import {login, logout} from "./store/authSlice"
 import { Footer, Header } from './Components'
@@ -26,8 +26,8 @@ function App() {
     <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
       <div className='w-full block'>
         <Header />
-        <main>
-        TODO:  <Outlet />
+        <main className='text-center'>
+        POSTS <Outlet />
         </main>
         <Footer />
       </div>
--- src/Components/Header/LogoutBtn.jsx
+++ src/Components/Header/LogoutBtn.jsx
@@ -1,3 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from '../../appwrite/auth'
+import { logout } from '../../store/authSlice'
+import { useNavigate } from 'react-router-dom'
+
+const LogoutBtn = () => {
+    const [username, setUsername] = useState('')
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        authService.getCurrentUser().then(user => {
+            setUsername(user.name) // Assuming 'user.name' contains the username
+        }).catch(err => {
+            console.error('Failed to fetch user', err)
+        })
+    }, [])
+
+    const logoutHandler = () => {
+        authService.logout().then(() => {
+            dispatch(logout())
+            navigate('/login')
+        })
+    }
+
+    return (
+        <div>
+            <button
+                className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                onClick={logoutHandler}
+            >
+                Logout
+            </button>
+            <p className='text-center'>{username}</p>
+        </div>
+    )
+}
+
+export default LogoutBtn
+
+/*
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import authService from '../../appwrite/auth'
@@ -26,3 +68,4 @@ const LogoutBtn = () => {
 }
 
 export default LogoutBtn
+*/
